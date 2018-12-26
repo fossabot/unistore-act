403223b963e32bbbb9ba6b986193fbb5c0b6bb92
WhiteTrefoil <whitetrefoil@gmail.com>
2018-12-26 13:40:21 +0800

    Compatible with @whitetrefoil/unistore-bind@v0.2.0

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-type IBoundAction<P> = (payload?: P) => void
+type BoundAction = () => void|Promise<void>
+type BoundActionWithArg = (payload: string) => void|Promise<void>
+type AnyBoundAction = BoundAction|BoundActionWithArg
 
-export function act(action: IBoundAction<string>|IBoundAction<void>) {
-  return (ev: Event) => (action as IBoundAction<string>)((ev.target as HTMLInputElement).value)
+
+export function act(action: AnyBoundAction) {
+  return (ev: Event) => action((ev.target as HTMLInputElement).value)
 }
 
-export function cact(action: IBoundAction<string>|IBoundAction<void>) {
-  return (ev: Event) => (action as IBoundAction<string>)((ev.currentTarget as HTMLInputElement).value)
+export function cact(action: AnyBoundAction) {
+  return (ev: Event) => action((ev.currentTarget as HTMLInputElement).value)
 }
